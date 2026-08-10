16c621fa2063aed4d94d6aa3df398e4eede076e0
Chidiogo <chidiogoezeh.me@gmail.com>
2026-08-10 16:26:34 +0100

    perform calculation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ fn main() {
     let first_number = 10;
     let second_number = 5;
 
+    let addition = first_number + second_number;
+    let subtraction = first_number - second_number;
+    let multiplication = first_number * second_number;
+    let division = first_number / second_number;
+
     println!("First number: {}", first_number);
-    println!("Second number: {}", second_number)
+    println!("Second number: {}", second_number);
+
+    println!("Addition: {}", addition);
+    println!("Subtraction: {}", subtraction);
+    println!("Multiplication: {}", multiplication);
+    println!("Division: {}", division);
 }
