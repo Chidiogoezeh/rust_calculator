9d9c13aa4efccb255384823a9a501573c73142f5
Chidiogo <chidiogoezeh.me@gmail.com>
2026-08-10 16:17:53 +0100

    practicing variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let first_number = 10;
+    let second_number = 5;
+
+    println!("First number: {}", first_number);
+    println!("Second number: {}", second_number)
 }
