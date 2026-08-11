097545297b61f97cbf7a5a143434e8c4f2d9bd24
Chidiogo <chidiogoezeh.me@gmail.com>
2026-08-11 19:01:22 +0100

    Create a function for each operation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 use std::io;
-fn main() {
-    let first_number = 10;
-    let second_number = 5;
 
-    let addition = first_number + second_number;
-    let subtraction = first_number - second_number;
-    let multiplication = first_number * second_number;
-    let division = first_number / second_number;
+// Addition function
+fn add(a: f64, b: f64) -> f64 {
+    a + b
+}
+
+// subtraction function
+fn subtract(a: f64, b: f64) -> f64 {
+    a - b
+}
+
+//Multiplication function
+fn multiply(a: f64, b: f64) -> f64 {
+    a * b
+}
+
+// Division function
+fn divide(a: f64, b: f64) -> f64 {
+    a / b
+}
+
+fn main() {
+    let first_number = 10.0;
+    let second_number = 5.0;
+
+    let addition = add(first_number, second_number);
+    let subtraction = subtract(first_number, second_number);
+    let multiplication = multiply(first_number, second_number);
+    let division = divide(first_number, second_number);
 
     println!("First number: {}", first_number);
     println!("Second number: {}", second_number);
